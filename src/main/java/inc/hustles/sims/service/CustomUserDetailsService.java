fb21e96003fb8d74d5461b66bfb2518eda4df39c
SimbaMupfu <scmupfururirwa@gmail.com>
2025-12-05 11:09:58 +0200

    Load the user using the username

--- src/main/java/inc/hustles/sims/service/CustomUserDetailsService.java
+++ src/main/java/inc/hustles/sims/service/CustomUserDetailsService.java
@@ -1,4 +1,30 @@
 package inc.hustles.sims.service;
 
-public class CustomUserDetailsService {
+import inc.hustles.sims.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final Map<String, User> users = new HashMap<>();
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = users.get(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
 }
